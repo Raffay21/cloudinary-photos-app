efa351e2cb3d9986f2026ee3800a03862f39ce92
Raffay21 <iamraffay@gmail.com>
2023-10-05 07:41:38 +0500

    New Project

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      serverActions: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
